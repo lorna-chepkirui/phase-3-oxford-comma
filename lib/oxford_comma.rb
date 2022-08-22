7c8b2fd74ac15edbc1da79ed1faf4ef87c9b69bd
lorna-chepkirui <lornachepkirui20@gmail.com>
2022-08-22 22:50:00 +0300

    commit tree d8baa57e22f6b08a7a8d2722e44ef1794e412bb4

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
-
-end
+def add_and(array, len)
+    array.slice(0, len-1) << array.pop().prepend("and ")
+  end
+  
+  def oxford_comma(array)
+    len = array.size;
+     if len == 1
+      array[0]
+    elsif len == 2
+      add_and(array, len).join(" ")
+    elsif len > 2
+      add_and(array, len).join(", ")
+    end
+  end
